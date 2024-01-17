e1b6e5c70e075770cfe0c0f7b945f016b24854f7
Chenery, Alfie <alfie.chenery20@imperial.ac.uk>
2024-01-17 01:05:36 +0000

    added camera controls

--- trace_generation/controller.py
+++ trace_generation/controller.py
@@ -2,7 +2,7 @@ from inputs import get_gamepad
 from math import pow
 import threading
 
-class XboxController(object): #test if needed
+class Controller():
     def __init__(self):
 
         self.TRIGGER_MAX_VALUE = pow(2, 8)
@@ -12,6 +12,8 @@ class XboxController(object): #test if needed
         self.LeftJoystickY = 0
         self.RightJoystickX = 0
         self.RightJoystickY = 0
+        self.LeftJoystickPress = 0
+        self.RightJoystickPress = 0
 
         self.LeftTrigger = 0
         self.RightTrigger = 0
@@ -23,7 +25,13 @@ class XboxController(object): #test if needed
         self.Y = 0
         self.B = 0
 
-        self.Start = 0
+        self.DPadLeft = 0
+        self.DPadRight = 0
+        self.DPadUp = 0
+        self.DPadDown = 0        
+
+        self.Start = 0 
+        self.Menu = 0
 
         self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
         self._monitor_thread.daemon = True
@@ -35,6 +43,8 @@ class XboxController(object): #test if needed
                 self.LeftJoystickY,
                 self.RightJoystickX,
                 self.RightJoystickY,
+                self.LeftJoystickPress,
+                self.RightJoystickPress,
                 self.LeftTrigger,
                 self.RightTrigger,
                 self.LeftBumper,
@@ -43,13 +53,21 @@ class XboxController(object): #test if needed
                 self.X,
                 self.Y,
                 self.B,
-                self.Start]
+                self.DPadLeft,
+                self.DPadRight,
+                self.DPadUp,
+                self.DPadDown,
+                self.Start,
+                self.Menu]
 
 
     def _monitor_controller(self):
         while True:
             events = get_gamepad()
             for event in events:
+
+                #print(event.code)
+
                 if event.code == 'ABS_X':
                     val = event.state / self.JOYSTICK_MAX_VALUE
                     self.LeftJoystickX = val if abs(val) >= 0.3 else 0 #deadzone to prevent drift
@@ -62,6 +80,10 @@ class XboxController(object): #test if needed
                 elif event.code == 'ABS_RY':
                     val = event.state / self.JOYSTICK_MAX_VALUE
                     self.RightJoystickY = val if abs(val) >= 0.3 else 0 #deadzone to prevent drift
+                elif event.code == 'BTN_THUMBL':
+                    self.LeftJoystickPress = event.state
+                elif event.code == 'BTN_THUMBR':
+                    self.RightJoystickPress = event.state
 
                 elif event.code == 'ABS_Z':
                     self.LeftTrigger = event.state / self.TRIGGER_MAX_VALUE
@@ -81,11 +103,20 @@ class XboxController(object): #test if needed
                 elif event.code == 'BTN_EAST':
                     self.B = event.state
 
+                elif event.code == 'ABS_HAT0X':
+                    self.DPadLeft = int(event.state == -1)
+                    self.DPadRight = int(event.state == 1)
+                elif event.code == 'ABS_HAT0Y':
+                    self.DPadUp = int(event.state == -1)
+                    self.DPadDown = int(event.state == 1)
+
                 elif event.code == 'BTN_START':
                     self.Start = event.state
+                elif event.code == 'BTN_SELECT':
+                    self.Menu = event.state
 
 
 if __name__ == '__main__':
-    controller = XboxController()
+    controller = Controller()
     while True:
         print(controller.readAll())
--- trace_generation/generate_trace.py
+++ trace_generation/generate_trace.py
@@ -3,8 +3,7 @@ import time
 import pybullet_data
 import pickle
 import os
-import keyboard
-from controller import XboxController
+from controller import Controller
  
 p.connect(p.GUI)
 p.resetSimulation()
@@ -13,7 +12,7 @@ p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
 armId = p.loadURDF("franka_panda/panda.urdf", [0,0,0], [0,0,0,1], useFixedBase=True)
 numJoints = p.getNumJoints(armId)
-controller = XboxController()
+controller = Controller()
 
 
 def Clamp(val, low, high):
@@ -49,6 +48,11 @@ def set_joint_positions(prev_positions, desired_positions, interpolation_steps=1
 
 
 def main():
+    cameraMoveSpeed = 5
+    cameraYaw = 50.0
+    cameraPitch = -35.0
+    cameraDist = 5.0
+
     current_joint = 0    #Id of joint we are currently controlling
 
     intermediate_pose = get_joint_positions() #Pose we are currently working on. Since we manipulate one joint
@@ -56,15 +60,20 @@ def main():
                                               # joints that we havent saved as a keyframe yet
 
     trace = [get_joint_positions()]    #The trace so far. trace[-1] is the last keyframe added
+    saveTrace = True
 
-    while not controller.Start and not keyboard.is_pressed("q"): #use start button to exit and save trace so far
+    while True:
 
         if controller.LeftBumper: #move to previous joint
+            p.setDebugObjectColor(armId, current_joint)
             current_joint = Clamp(current_joint - 1, 0, numJoints - 1)
-
+            p.setDebugObjectColor(armId, current_joint, objectDebugColorRGB=[255,0,0])
+            
         if controller.RightBumper: #move to next joint
+            p.setDebugObjectColor(armId, current_joint)
             current_joint = Clamp(current_joint + 1, 0, numJoints - 1)
-
+            p.setDebugObjectColor(armId, current_joint, objectDebugColorRGB=[255,0,0])
+            
         if controller.Y: #cancel this joint's movement but keep other joints, revert to intermediate_pose
             set_joint_positions(get_joint_positions(), intermediate_pose)
 
@@ -75,16 +84,36 @@ def main():
         if controller.X: #save this joint to intermediate_pose
             intermediate_pose = get_joint_positions()
 
-        if controller.A or keyboard.is_pressed("space"): #save this position as a key frame
+        if controller.A: #save this position as a key frame
             trace.append(get_joint_positions())
             intermediate_pose = get_joint_positions()
-            print("!!!!!!!!!!!!!!!!!!")
 
+        if controller.DPadUp:
+            p.setDebugObjectColor(armId, current_joint, objectDebugColorRGB=[255,0,0])
+            p.configureDebugVisualizer(p.COV_ENABLE_WIREFRAME, 1)
+
+        if controller.DPadDown:
+            p.configureDebugVisualizer(p.COV_ENABLE_WIREFRAME, 0)
+
+        #eft joystick controls joints
         joints = get_joint_positions()
         joints[current_joint] += controller.LeftJoystickY
         set_joint_positions(get_joint_positions(), joints, interpolation_steps=5)
 
-        print(f"Currently controlling joint {current_joint}")
+        #right joystick controlls camera
+        cameraYaw += cameraMoveSpeed * controller.RightJoystickX
+        cameraPitch += cameraMoveSpeed * controller.RightJoystickY
+        cameraDist += controller.LeftTrigger
+        cameraDist -= controller.RightTrigger
+        p.resetDebugVisualizerCamera(cameraDist, cameraYaw, cameraPitch, [0,0,0])
+
+        if controller.Start: #quit without saving
+            saveTrace = False
+            break
+
+        if controller.Menu: #save and quit
+            saveTrace = True
+            break
             
 
         p.stepSimulation()
@@ -93,15 +122,15 @@ def main():
 
     p.disconnect()
 
-    
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "trace.pkl")
-    with open(path, 'wb') as f:
-        pickle.dump(trace, f)
-
     print("")
     print(trace)
+    
+    if saveTrace:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "trace.pkl")
+        with open(path, 'wb') as f:
+            pickle.dump(trace, f)
 
-
+    
 if __name__ == "__main__":
     main()
 
