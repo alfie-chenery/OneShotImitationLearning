3804670295af2270058b28c013526c63b72944cc
Chenery, Alfie <alfie.chenery20@imperial.ac.uk>
2024-04-10 11:53:18 +0100

    dinobot

--- camera_test.py
+++ camera_test.py
@@ -3,12 +3,12 @@ import pybullet as p
 import pybullet_data
 import time
 import keyboard
+from matplotlib import pyplot as plt
+from PIL import Image
 
 p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
 p.setGravity(0, 0, -10)
-
 
 plane_id = p.loadURDF("plane.urdf")
 armId = p.loadURDF("franka_panda/panda.urdf", [0,0,0], [0,0,0,1], useFixedBase=True)
@@ -16,8 +16,10 @@ obj1Id = p.loadURDF("quadruped/quadruped.urdf", [0,0.1,0.3], [0,0,0,1])
 numJoints = p.getNumJoints(armId)
 end_effector_id = numJoints - 1
 
+img_size = 1000
 fov, aspect, nearplane, farplane = 60, 1.0, 0.01, 100
 projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, nearplane, farplane)
+
 def get_camera_snapshot():
     pos, orn, _, _, _, _ = p.getLinkState(armId, end_effector_id, computeForwardKinematics=True)
     rotation_matrix = p.getMatrixFromQuaternion(orn)
@@ -29,14 +31,28 @@ def get_camera_snapshot():
     camera_vector = rotation_matrix.dot(init_camera_vector)
     up_vector = rotation_matrix.dot(init_up_vector)
     view_matrix = p.computeViewMatrix(pos, pos + 0.1 * camera_vector, up_vector)
-    img = p.getCameraImage(1000, 1000, view_matrix, projection_matrix)
+    img = p.getCameraImage(img_size, img_size, view_matrix, projection_matrix)
     return img
+
+def save_snapshot(img):
+    #img = p.getCameraImage(224, 224, shadow = False, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+    rgb_opengl = (np.reshape(img[2], (img_size, img_size, 4)))
+    depth_buffer_opengl = np.reshape(img[3], [img_size, img_size])
+    depth_opengl = farplane * nearplane / (farplane - (farplane - nearplane) * depth_buffer_opengl)
+    seg_opengl = np.reshape(img[4], [img_size, img_size]) * 1. / 255.
+
+    rgbim = Image.fromarray(rgb_opengl)
+    rgbim_no_alpha = rgbim.convert('RGB')
+
+    rgbim_no_alpha.save('rgb.jpg')
+    plt.imsave('depth.jpg', depth_buffer_opengl)
 
 # Main loop
 while True:
     p.stepSimulation()
 
-    if keyboard.is_pressed('space') or True:
-        get_camera_snapshot()
+    if keyboard.is_pressed('space'):
+        img = get_camera_snapshot()
+        save_snapshot(img)
     
     time.sleep(1./240.)
---
+++ dinobot/alignment_DINO.py
@@ -0,0 +1,57 @@
+#Install this DINO repo to extract correspondences: https://github.com/ShirAmir/dino-vit-features
+
+import numpy as np
+import torch
+from dinofeatures.correspondences import find_correspondences
+
+def find_transformation(X, Y):
+    #Find transformation given two sets of correspondences between 3D points
+    # Calculate centroids
+    cX = np.mean(X, axis=0)
+    cY = np.mean(Y, axis=0)
+    # Subtract centroids to obtain centered sets of points
+    Xc = X - cX
+    Yc = Y - cY
+    # Calculate covariance matrix
+    C = np.dot(Xc.T, Yc)
+    # Compute SVD
+    U, S, Vt = np.linalg.svd(C)
+    # Determine rotation matrix
+    R = np.dot(Vt.T, U.T)
+    # Determine translation vector
+    t = cY - np.dot(R, cX)
+    return R, t
+
+
+#Hyperparameters for DINO correspondences extraction
+num_pairs = 8
+load_size = 224
+layer = 9
+facet = 'key'
+bin=True
+thresh=0.05
+model_type='dino_vitb8'
+stride=4
+
+if __name__ == "__main__":
+    
+ #Get rgbd from wrist camera.
+    rgb_bn, depth_bn = camera.get_rgbd()
+    Error = 100000
+    ERR_THRESHOLD = 50 #A generic error between the two sets of points
+    while error > ERR_THRESHOLD:
+        rgb_live, depth_live = camera.get_rgbd()
+        with torch.no_grad():
+            points1, points2, image1_pil, image2_pil = find_correspondences(rgb_live, rgb_bn, num_pairs, load_size, layer,
+                                                                               facet, bin, thresh, model_type, stride)
+            #Given the pixel coordinates of the correspondences, add the depth channel
+            points1 = add_depth(points1, depth_bn)
+            points2 = add_depth(points2, depth_live)
+            R, t = find_transformation(points1, points2)
+
+            #A function to convert pixel distance into meters based on calibration of camera.
+            t_meters = convert_pixels_to_meters(t)
+
+            #Move robot
+            robot.move(t_meters,R)
+            error = compute_error(points1, points2)
