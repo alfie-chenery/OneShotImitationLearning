a9bc38ea7ea8d25536d168f5f6a1f155e8eb216d
Alfie Chenery <alfie@introversion.co.uk>
2024-04-11 13:27:42 +0100

    start plugging stuff together

--- main_code/environment.py
+++ main_code/environment.py
@@ -17,15 +17,16 @@ class FrankaArmEnvironment:
         #set object variables
         self.robotId = p.loadURDF("franka_panda/panda.urdf", [0,0,0], [0,0,0,1], useFixedBase=True)
         self.planeId = p.loadURDF("plane.urdf")
-        self.eefId = p.getNumJoints(self.robotId) - 1
+        self.numJoints = p.getNumJoints(self.robotId)
+        self.eefId = self.numJoints - 1
         self.obj1Id = p.loadURDF("quadruped/quadruped.urdf", [0,0.1,0.3], [0,0,0,1])
 
-        self.img_size = 1000
+        self.imgSize = 1000
         self.fov = 60
         self.aspect = 1.0
         self.nearplane = 0.01
         self.farplane = 100
-        self.projection_matrix = p.computeProjectionMatrixFOV(self.fov, self.aspect, self.nearplane, self.farplane)
+        self.projectionMatrix = p.computeProjectionMatrixFOV(self.fov, self.aspect, self.nearplane, self.farplane)
 
 
     def stepEnv(self):
@@ -35,8 +36,26 @@ class FrankaArmEnvironment:
 
     def closeEnv(self):
         #save relevant stuff
-        #close simulation
-        pass
+        p.disconnect()
+
+
+    def getJointAngles(self):
+        return [p.getJointState(self.robotId, i)[0] for i in range(self.numJoints)]
+    
+
+    def setJointAngles(self, desiredAngles, interpolationSteps=100):
+        for i in range(interpolationSteps):
+            alpha = (i+1) / interpolationSteps
+            interpolatedPosition = [(1 - alpha) * prev + alpha * desired for prev, desired in zip(self.getJointAngles(), desiredAngles)]
+            forces = [500.0] * len(interpolatedPosition)
+
+            p.setJointMotorControlArray(self.robotId, 
+                                        range(len(interpolatedPosition)),
+                                        p.POSITION_CONTROL,
+                                        targetPositions=interpolatedPosition,
+                                        forces=forces)
+            
+            #self.stepEnv()
 
 
     def robotGetCameraSnapshot(self):
@@ -50,7 +69,7 @@ class FrankaArmEnvironment:
         camera_vector = rotation_matrix.dot(init_camera_vector)
         up_vector = rotation_matrix.dot(init_up_vector)
         view_matrix = p.computeViewMatrix(pos, pos + 0.1 * camera_vector, up_vector)
-        img = p.getCameraImage(self.img_size, self.img_size, view_matrix, self.projection_matrix)
+        img = p.getCameraImage(self.imgSize, self.imgSize, view_matrix, self.projectionMatrix)
         return img
 
 
@@ -66,3 +85,15 @@ class FrankaArmEnvironment:
 
     #     rgbim_no_alpha.save('rgb.jpg')
     #     plt.imsave('depth.jpg', depth_buffer_opengl)
+
+
+    def setCameraPos(self, cameraDist, cameraYaw, cameraPitch):
+        p.resetDebugVisualizerCamera(cameraDist, cameraYaw, cameraPitch, [0,0,0])
+
+
+    def enableWireframe(self):
+        p.configureDebugVisualizer(p.COV_ENABLE_WIREFRAME, 1)
+
+
+    def disableWireframe(self):
+        p.configureDebugVisualizer(p.COV_ENABLE_WIREFRAME, 0)
--- main_code/main.py
+++ main_code/main.py
@@ -1,10 +1,86 @@
 import environment
-import keyboard
+import torch
+import numpy as np 
+import torchvision.transforms as T
+from PIL import Image
+import glob
+import os
 
+
+#Hyperparameters for DINO correspondences extraction
+num_pairs = 8
+load_size = 224
+layer = 9
+facet = 'key'
+bin=True
+thresh=0.05
+model_type='dino_vitb8'
+stride=4
+
+
+#Download and load DINO
+dino = torch.hub.load('facebookresearch/dino:main', 'dino_vitb8')
+
+#Create an image transform pipeline
+image_transforms = T.Compose([
+    T.Resize(256, interpolation=T.InterpolationMode.BICUBIC),
+    T.CenterCrop(224),
+    T.ToTensor(),
+    T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+])
+
+imageEmbeddings = {}
+
+
+def loadImages():
+    #Load saved images and embed using transformer
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    i = 0
+    for path in glob.glob(dir_path + "\\image_snapshots\\*.jpg"):
+        img = Image.open(path)
+        img = image_transforms(img)
+        img = img.unsqueeze(0)
+        emb = dino(img)
+
+        imageEmbeddings[path.split("\\")[-1].split(".")[0]] = emb
+        i += 1
+    
+    print(f"Successfully loaded {i} images")
+
+
+def find_transformation(X, Y):
+    #Find transformation given two sets of correspondences between 3D points
+    # Calculate centroids
+    cX = np.mean(X, axis=0)
+    cY = np.mean(Y, axis=0)
+    # Subtract centroids to obtain centered sets of points
+    Xc = X - cX
+    Yc = Y - cY
+    # Calculate covariance matrix
+    C = np.dot(Xc.T, Yc)
+    # Compute SVD
+    U, S, Vt = np.linalg.svd(C)
+    # Determine rotation matrix
+    R = np.dot(Vt.T, U.T)
+    # Determine translation vector
+    t = cY - np.dot(R, cX)
+    return R, t
+
+
+#Create a cosine similarity object
+cos_sim = torch.nn.CosineSimilarity(dim=1, eps=1e-08)
+
+loadImages()
+
+#Compute cosine similarities
+for a in ["mouse", "bottle"]:
+    for b in ["wallet", "rat"]:
+        print(a,b)
+        print(cos_sim(imageEmbeddings[a], imageEmbeddings[b]))
+
+#Robot arm environment
 env = environment.FrankaArmEnvironment()
 
 while True:
-    if keyboard.is_pressed('space'):
-            img = env.robotGetCameraSnapshot()
-            #self.save_snapshot(img)
     env.stepEnv()
